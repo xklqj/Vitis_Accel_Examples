83a817fafa1d3ff11d7a6f6a5ae77e9a9432e9f9
xklqj <53899492+xklqj@users.noreply.github.com>
2020-04-27 15:33:19 +0800

    Update host.cpp

--- host/streaming_simple/src/host.cpp
+++ host/streaming_simple/src/host.cpp
@@ -1,6 +1,6 @@
 /**********
 Copyright (c) 2019, Xilinx, Inc.
-All rights reserved.
+All rights reserve
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
